51da54d9ac4a0254c8ddfba07cb5b6660db03d5d
akit3ru <svetagvozdikina2006@gmail.com>
2023-06-16 16:26:30 +0200

    initial

---
+++ 4_1.py
@@ -0,0 +1,44 @@
+"""
+def strcounter(s):
+    # O (N*M)
+    for sym in set(s):
+        count = 0
+        for sub_sym in s:
+            if sym == sub_sym:
+                count += 1
+        print(sym, count)
+
+
+strcounter('abcaddf')
+# N = 7 кол-во символов
+# М = 5 кол-во уникальных символов
+# S = 5*7 = 35 асимптотическая сложность
+# O - кол-во операций
+
+
+def strcounter(s):
+    # O (N**2)
+    for sym in s:
+        count = 0
+        for sub_sym in s:
+            if sym == sub_sym:
+                count += 1
+        print(sym, count)
+
+
+# S = 7*7 = 49
+strcounter('abcaddf')
+"""
+
+
+def strcounter(s):
+    # O(N)
+    syms_counter = {}
+    for sym in s:
+        syms_counter[sym] = syms_counter.get(sym, 0) + 1
+    for sym, count in syms_counter.items():
+        print(sym, count)
+
+
+strcounter('abcaddf')
+# N = 10; O = 10
